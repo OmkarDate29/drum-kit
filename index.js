6cd100bcc6cad3f97442b8c4d089f9932eb106eb
Omkar Date <omkardate29@gmail.com>
2022-12-08 23:52:34 +0530

    Some minor changes in JavaScript file

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
-// /* 'keydown' is event-listner that connects keyboard and webpage */
-// document.addEventListener("keydown", function (event) {
-//   // 'event' returns properties of key that get pressed
-//   // here 'event' is just a passing parameter variable that means it can be anything
-//   // from 'event.key' => '.key' is one of that property,
-//   // that returns which key is pressed and this return value is in string form
-//   if (event.key === "1") {
-//     alert("Key is pressed!!!");
-//   }
-// });
-
-/* 
-// this is how we can play sound
-var audio = new Audio("sounds/crash.mp3");
-// 'Audio' is constructor that takes file location as input and
-// has lot of properties including '.play'
-audio.play();
- */
-
-/* this detects which image is pressed */
 var btnLength = document.querySelectorAll(".drum").length;
 for (let i = 0; i < btnLength; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    // above statement works as address to the image clicked
-    // console.log(this);
-
-    // var btnInnerHTML = this.innerHTML;
-    // makeSound(btnInnerHTML);
-
     makeSound(this.innerHTML);
   });
 }
 
-/* this plays sond on key pressed */
 document.addEventListener("keydown", function (event) {
-  // console.log(event);
   makeSound(event.key);
 });
 
-/* this assigns sound to the image or key pressed */
 function makeSound(key) {
   var audio;
   switch (key) {
